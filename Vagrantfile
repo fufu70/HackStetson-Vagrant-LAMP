9def40f4ff6540058c8ff5788fedc0a45d30a16a
Christian Micklisch <christian@beachfrontmedia.com>
2016-04-07 08:56:36 -0400

    first commit for vagrant machine

---
+++ Vagrantfile
@@ -0,0 +1,20 @@
+Vagrant.configure(2) do |config|
+
+	config.vm.box = "precise32"
+
+	config.vm.box_url = "http://files.vagrantup.com/precise32.box"
+	# Mentioning the SSH Username/Password:
+	config.ssh.username = "vagrant"
+	config.ssh.password = "vagrant"
+
+	config.vm.synced_folder "src/", "/var/www/website"
+
+	# Begin Configuring
+	config.vm.define "lamp" do|lamp|
+		lamp.vm.hostname = "lamp" # Setting up hostname
+		lamp.vm.network "private_network", ip: "192.168.205.10" # Setting up machine's IP Address
+		lamp.vm.provision :shell, path: "setup.sh" # Provisioning with script.sh
+	end
+
+	# End Configuring
+end
---
+++ setup.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+# Updating repository
+
+sudo apt-get -y update
+
+# Installing Apache
+
+sudo apt-get -y install apache2
+
+# Installing MySQL and it's dependencies, Also, setting up root password for MySQL as it will prompt to enter the password during installation
+
+sudo debconf-set-selections <<< 'mysql-server-5.5 mysql-server/root_password password rootpass'
+sudo debconf-set-selections <<< 'mysql-server-5.5 mysql-server/root_password_again password rootpass'
+sudo apt-get -y install mysql-server libapache2-mod-auth-mysql php5-mysql
+
+# Installing PHP and it's dependencies
+sudo apt-get -y install php5 libapache2-mod-php5 php5-mcrypt
+
+#install vim ... cause why not
+sudo apt-get -y install vim nmap
+
+# Setup root directory for apache to be /var/www/website
+sudo sed -i 's/\/var\/www/\/var\/www\/website/g' /etc/apache2/sites-enabled/000-default
+
+# Restart apache server after updating its root directory
+sudo service apache2 restart
